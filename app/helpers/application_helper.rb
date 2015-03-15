05e8ed483557b9e24fc8f8cea1e511ae59eb80e4
alekseenkoss <alekseenkoss@gmail.com>
2015-03-15 12:38:50 +0600

    fix coderay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,8 +16,7 @@ module ApplicationHelper
   end
 end
 
-def coderay(text)
-  text =~ /\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m
+def coderay(text) 
   text.gsub!(/\<code(?: lang="(.+?)")?\>(.+?)\<\/code\>/m) do 
     code = CodeRay.scan($2, $1).div(:css => :class)
     "<notextile>#{code}</notextile>" 
